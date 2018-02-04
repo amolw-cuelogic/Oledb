4b3c761178eeaaa5efc9e23084b9c18bb9c39782
amolw-cuelogic <amol.wabale@cuelogic.com>
2018-02-04 21:36:09 +0530

    Initial commit

---
+++ MySqlConnection/MySqlConnection/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+
+namespace MySqlConnectionEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myConnectionString = "";
+            if (myConnectionString == "")
+            {
+                myConnectionString = "Database=world;Data Source=localhost;User Id=root;Password=Cuelogic";
+            }
+            MySqlConnection myConnection = new MySqlConnection(myConnectionString);
+            string myInsertQuery = "select * from city";
+            MySqlCommand myCommand = new MySqlCommand(myInsertQuery);
+            myCommand.Connection = myConnection;
+
+            MySqlDataAdapter dt = new MySqlDataAdapter(myCommand);
+            var ds = new DataSet();
+            
+            myConnection.Open();
+            dt.Fill(ds);
+            myCommand.Connection.Close();
+        }
+    }
+}
---
+++ MySqlConnectionOledb/MySqlConnectionOledb/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+
+namespace MySqlConnectionOledb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //OleDbConnection conn = new OleDbConnection(
+            // "Provider=MySQLProv;" +
+            // "Data Source=localhost;" +
+            // "User id=root;" +
+            // "Password=Cuelogic;"
+            //);
+
+            OleDbConnection conn = new OleDbConnection("Provider=MySQLProv;Server=localhost;Database=world;Uid=sa;Pwd=Cuelogic;");
+            string queryString = "SELECT * from world";
+            OleDbCommand command = new OleDbCommand(queryString, conn);
+            try
+            {
+                OleDbDataAdapter dt = new OleDbDataAdapter(command);
+                var ds = new DataSet();
+
+                conn.Open();
+                dt.Fill(ds);
+
+            }
+            catch (Exception)
+            {
+                conn.Close();
+                throw;
+            }
+        }
+    }
+}
